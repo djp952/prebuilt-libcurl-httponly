5178bdd05e637a01124343652db68f4fb99bd2c2
djp952 <djp952@gmail.com>
2020-03-04 22:32:18 -0500

    libcurl 7.69.0

--- src/scripts/contrithanks.sh
+++ src/scripts/contrithanks.sh
@@ -28,14 +28,31 @@
 
 start=$1
 
-if test -z "$start"; then
+if test "$start" = "-h"; then
   echo "Usage: $0 <since this tag/hash>"
+  exit
+fi
+if test -z "$start"; then
+  start=`git tag --sort=taggerdate | tail -1`;
+fi
+
+
+# We also include curl-www if possible. Override by setting CURLWWW
+if [ -z "$CURLWWW" ] ; then
+    CURLWWW=../curl-www
 fi
 
 cat ./docs/THANKS
 
 (
-git log --use-mailmap $start..HEAD | \
+ (
+  git log --use-mailmap $start..HEAD
+  if [ -d "$CURLWWW" ]
+  then
+   git -C ../curl-www log --use-mailmap $start..HEAD
+  fi
+ ) | \
+
 egrep -ai '(^Author|^Commit|by):' | \
 cut -d: -f2- | \
 cut '-d(' -f1 | \
